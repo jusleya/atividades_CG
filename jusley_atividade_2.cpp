3ce172dc1200a5b1d7fe22e26017224668508a53
Jusley Arley <jusley@getinfo.net.br>
2019-07-08 01:03:10 -0300

    Adiçao da condiçao de t1 e t2, gravaçao no arquivo de imagem

--- jusley_atividade_2.cpp
+++ jusley_atividade_2.cpp
@@ -14,7 +14,10 @@ struct p {
 	float b;
 };
 
-int main () {
+int main() {
+	FILE* output = fopen("imagem.ppm", "w");
+	fprintf(output, "P3\n");
+	
 	//Pontos
 	vector<float> p1, p2;
 	//valores da visualizacao em x e y
@@ -26,12 +29,6 @@ int main () {
 	//valores de delta
 	float deltax=0, deltay=0;
 	
-	//funcao d:
-	vector<int> op, d;
-	vector<float> e;
-	float a=0, b=0, c=0;
-	float t1=0, t2=0, r=0, y=0;
-	
 	printf("Informe o valor da fov:\n");
 	scanf("%f", &fov);
 	rad_fov = ((fov * 3.14)/180);
@@ -41,6 +38,7 @@ int main () {
 	
 	printf("Informe o valor para resx e resy:\n");
 	scanf("%d %d", &resx, &resy);
+	fprintf(output, "%d %d\n", resx, resy);
 	
 	ty= tan(fov/2)*(2*n);
 	tx= (ty*resx)/resy;
@@ -50,7 +48,10 @@ int main () {
 	aux_2= -(ty/2);
 	p1.push_back(aux_1);
 	p1.push_back(aux_2);
-	p1.push_back(n);
+	p1.push_back(-n);
+	p2.push_back(-aux_1);
+	p2.push_back(-aux_2);
+	p2.push_back(-n);
 	
 	deltax= ((p1[0]-p2[0])/resx);
 	deltay= ((p1[1]-p2[1])/resy);
@@ -69,6 +70,12 @@ int main () {
 			p[i][j].b = o[2] + (i+1)*(0) + 0;
 		}
 	}
+	//funcao d:
+	vector<int> op;
+	vector<float> e, t1, t2;
+	float a=0, b=0, c=0;
+	float r=0, y=0;
+	int d[3];
 	
 	//funcao d:
 	for(int i=0; i<3; i++) op.push_back(0), e.push_back(0);
@@ -76,17 +83,47 @@ int main () {
 	scanf("%f", &r);
 	// d vai receber os valores de pij. faca um for pra pecorrer pij e salve em d,
 	// depois veja se t1 e t2 sao valores validos para o hist, se for, pinta de branco, senao preto
-	a=(d[0]*d[0])+(d[1]*d[1])+(d[2]*d[2]);
-	b=(d[0]*(e[0]-op[0]))+(d[1]*(e[1]-op[1]))+(d[2]*(e[2]-op[2]));
-	b=2*b;
-	c=((e[0]-op[0])*(e[0]-op[0]))+((e[1]-op[1])*(e[1]-op[1]))+((e[2]-op[2])*(e[2]-op[2]));
-	c=c-(r*r);
 	
-	y=(b*b)-(4*a*c);
+	struct p img[resx][resy];
 	
-	t1=(((-b)+sqrt(y))/(2*a));
-	t2=(((-b)-sqrt(y))/(2*a));
-	printf("Valor de t': %.2f, t'': %.2f.\n", t1, t2);
+	for(int i=0; i<resx; i++){
+		for(int j=0; j<resy; j++){
+			float aux1=0, aux2=0;
+			d[0]=p[i][j].r;
+			d[1]=p[i][j].g;
+			d[2]=p[i][j].b;
+			
+			a=(d[0]*d[0])+(d[1]*d[1])+(d[2]*d[2]);
+			b=(d[0]*(e[0]-op[0]))+(d[1]*(e[1]-op[1]))+(d[2]*(e[2]-op[2]));
+			b=2*b;
+			c=((e[0]-op[0])*(e[0]-op[0]))+((e[1]-op[1])*(e[1]-op[1]))+((e[2]-op[2])*(e[2]-op[2]));
+			c=c-(r*r);
+			y=(b*b)-(4*a*c);
 	
+			aux1=(((-b)+sqrt(y))/(2*a));
+			aux2=(((-b)-sqrt(y))/(2*a));
+			t1.push_back(aux1);
+			t2.push_back(aux2);
+			if((t1[j]>=0 && t2[j]>=0) || t1[j]>=0 || t2[j]>=0){
+				img[i][j].r=255;
+				img[i][j].g=255;
+				img[i][j].b=255;
+			}
+			else {
+				img[i][j].r=0;
+				img[i][j].g=0;
+				img[i][j].b=0;
+			}		
+		}
+	}
+	
+	for(int i=0; i<resx; i++){
+		for(int j=0; j<resy; j++){
+			fprintf(output, "%.0f %.0f %.0f", img[i][j].r, img[i][j].g, img[i][j].b);
+		}
+		fprintf(output, "\n");
+	}
+
+	//for(int i=0; i<resx; i++) printf("Valor de t': %.2f, t'': %.2f.\n", t1[i], t2[i]);
 	return 0;
 }
